222ac1c7fb3edd224e14603eae61d1cc815d79d6
dasda <das>
2020-03-25 17:43:51 +0300

    1

--- php/send.php
+++ php/send.php
@@ -1,5 +1,11 @@
 <?php
 
-    print_r($_POST);
-    
+    header('Content-Type: text/html; charset=UTF-8');
+    $link = mysql_connect('localhost', 'u16671', '3137204');
+    if (!$link) {
+        die('Ошибка соединения: ' . mysql_error());
+    }
+    echo 'Успешно соединились';
+    mysql_close($link);
+
 ?>
