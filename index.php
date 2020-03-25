a4c0d6fe3123f066ae0c768a7f05b445997eb771
me <me>
2020-03-25 20:15:55 +0300

    Finish!

--- index.php
+++ index.php
@@ -6,9 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Задание 3</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
-    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
     <script defer src="https://code.jquery.com/jquery-3.4.1.min.js"
         integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script defer src="js/main.js"></script>
@@ -17,94 +16,70 @@
 <body>
     <form id="form" action="php/send.php" method="POST">
         <!-- Текстовое поле для имени -->
-        <div class="field">
-            <label class="label" for="name-input">Имя</label>
-            <div class="control">
-                <input id="name-input" name="name" class="input is-primary" type="text" placeholder="Ваше имя">
-            </div>
+        <div class="form-group">
+            <label for="name-input">Имя</label>
+            <input id="name-input" name="name" class="form-control" type="text" placeholder="Ваше имя">
         </div>
         <!-- Текстовое поле для почты -->
-        <div class="field">
-            <label for="form-email" class="label">Email</label>
-            <div class="control">
-                <input id="form-email" name="email" class="input is-primary" type="email" placeholder="Ваш email">
-            </div>
+        <div class="form-group">
+            <label for="form-email">Email</label>
+            <input id="form-email" name="email" class="form-control" type="email" placeholder="Ваш email">
         </div>
         <!-- Выбор из списка для года рождения -->
-        <div class="field">
+        <div class="form-group">
             <label for="age-select" class="label">Год рождения</label>
-            <div class="control">
-                <div class="select is-primary">
-                    <select name="age" id="age-select"></select>
-                </div>
-            </div>
+            <select name="age" id="age-select" class="custom-select"></select>
         </div>
         <!-- Радиокнопки для пола -->
-        <div class="field">
-            <label class="label">Пол</label>
-            <div class="control">
-                <label class="radio">
-                    <input type="radio" name="sex" value="male" checked>
-                    Мужской
-                </label>
-                <label class="radio">
-                    <input type="radio" name="sex" value="female">
-                    Женский
-                </label>
-            </div>
+        <div class="form-group">
+            <label class="label">Пол</label><br>
+            <label class="radio">
+                <input type="radio" name="sex" value="male" checked>
+                Мужской
+            </label>
+            <label class="radio">
+                <input type="radio" name="sex" value="female">
+                Женский
+            </label>
         </div>
         <!-- Радиокнопки для количества конечностей -->
-        <div class="field">
-            <label class="label">Количество конечностей</label>
-            <div class="control">
-                <label class="radio">
-                    <input type="radio" name="limbs" checked value="4">
-                    4
-                </label>
-                <label class="radio">
-                    <input type="radio" name="limbs" value="4">
-                    6
-                </label>
-                <label class="radio">
-                    <input type="radio" name="limbs" value="4">
-                    8
-                </label>
-            </div>
+        <div class="form-group">
+            <label>Количество конечностей</label><br>
+            <label class="radio">
+                <input type="radio" name="limbs" checked value="4">
+                4
+            </label>
+            <label class="radio">
+                <input type="radio" name="limbs" value="6">
+                6
+            </label>
+            <label class="radio">
+                <input type="radio" name="limbs" value="8">
+                8
+            </label>
         </div>
         <!-- Множественный выбор сверхспособностей -->
-        <div class="field">
-            <label for="form-select" class="label">Сверхспособности</label>
-            <div class="control">
-                <div class="select is-multiple is-primary">
-                    <select id="form-select" multiple size="3" name="powers[]">
-                        <option value="immortality" selected>Бессмертие</option>
-                        <option value="levitation">Левитация</option>
-                        <option value="walls-walking">Хождение сквозь стены</option>
-                    </select>
-                </div>
-            </div>
+        <div class="form-group">
+            <label for="form-select">Сверхспособности</label>
+            <select id="form-select" class="form-control" multiple size="3" name="powers[]">
+                <option value="immortality" selected>Бессмертие</option>
+                <option value="levitation">Левитация</option>
+                <option value="walls-walking">Хождение сквозь стены</option>
+            </select>
         </div>
         <!-- Текстовое поле для биографии -->
-        <div class="field">
+        <div class="form-group">
             <label for="form-text" class="label">Биография</label>
-            <div class="control">
-                <textarea id="form-text" name="bio" class="textarea  is-primary"
+            <textarea id="form-text" name="bio" class="form-control"
                     placeholder="Напишите здесь немного о себе..."></textarea>
-            </div>
         </div>
         <!-- Чекбокс -->
-        <div class="field">
-            <div class="control">
-                <label class="checkbox">
-                    <input type="checkbox" name="check" checked>
-                    С <a href="#" class="has-text-primary">контрактом</a> ознакомлен(а).
-                </label>
-            </div>
+        <div class="custom-control custom-checkbox">
+          <input type="checkbox" name="check" class="custom-control-input" id="checkbox-input" checked>
+          <label class="custom-control-label" for="checkbox-input">С <a href="#" class="has-text-primary">контрактом</a> ознакомлен(а).</label>
         </div>
         <!-- Кнопка отправить -->
-        <div class="control">
-            <button id="btn" type="submit" class="button is-light is-primary">Отправить</button>
-        </div>
+        <button id="btn" type="submit" class="mt-2 btn btn-primary">Отправить</button>
     </form>
 </body>
 
